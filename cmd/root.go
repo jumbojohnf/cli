a51a25edb397d9ae056de1857c52a6c5731d01df
Yi Wang <neakor@gmail.com>
2022-04-18 12:18:38 -0700

    Add root command

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/funcgql/cli/cmd/aws"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "funcgql",
+	Short: "A CLI for writing a federated GQL application on serverless cloud providers",
+	Long:  "The CLI can be configured via a funcgql.json file within the Git repository",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(aws.AWSCmd)
+}
