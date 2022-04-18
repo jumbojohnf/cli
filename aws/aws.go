e38f7d6224e3ad8cdfbf9362205a1c05c6402a9e
Yi Wang <neakor@gmail.com>
2022-04-18 12:01:54 -0700

    Add AWS create lambda role function

--- aws/aws.go
+++ aws/aws.go
@@ -1,1 +1,32 @@
+package aws
+
 import (
+	"github.com/funcgql/cli/config"
+
+	"github.com/funcgql/cli/shell"
+	"github.com/pkg/errors"
+)
+
+type AWS struct {
+	binaryPath string
+	cfg        config.AWSConfig
+}
+
+func NewAPI() (*AWS, error) {
+	cfg, err := config.LoadFromRepoRoot()
+	if err != nil {
+		return nil, err
+	} else if cfg.AWS == nil {
+		return nil, errors.New("missing AWS configuration")
+	}
+
+	const binaryPath = "aws"
+	return &AWS{
+		binaryPath: binaryPath,
+		cfg:        *cfg.AWS,
+	}, nil
+}
+
+func (r *AWS) execute(args ...string) (shell.Output, error) {
+	return shell.Execute(r.binaryPath, args...)
+}
---
+++ aws/permissions.go
@@ -0,0 +1,46 @@
+package aws
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+
+	"github.com/funcgql/cli/cliio"
+	"github.com/pkg/errors"
+)
+
+//go:embed lambda-trust-policy.json
+var trustPolicyData []byte
+
+func (a *AWS) CreateLambdaRole() error {
+	fmt.Println("👮‍♀️ Creating lambda execution role")
+
+	policyFile, err := cliio.TempFile("policy")
+	if err != nil {
+		return err
+	}
+	if err := policyFile.WriteBytes(trustPolicyData); err != nil {
+		return errors.Wrap(err, "failed to export lambda role policy content")
+	}
+
+	if output, err := a.execute(
+		"iam", "create-role",
+		"--role-name", a.cfg.Lambda.RoleName,
+		"--assume-role-policy-document", fmt.Sprintf("file://%s", policyFile.AbsPath()),
+	); err != nil {
+		if output.ExitCode != 254 &&
+			!strings.Contains(output.Combined, fmt.Sprintf("%s already exists", a.cfg.Lambda.RoleName)) {
+			return errors.Wrapf(err, "failed to create lambda execution role %s", output.Combined)
+		}
+	}
+
+	if output, err := a.execute(
+		"iam", "attach-role-policy",
+		"--role-name", a.cfg.Lambda.RoleName,
+		"--policy-arn", "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+	); err != nil {
+		return errors.Wrapf(err, "failed to attach lambda role policy %s", output.Combined)
+	}
+
+	return nil
+}
