3078fe2650ebdcb5b93117409c9c129baf8b9091
Yi Wang <neakor@gmail.com>
2022-04-21 20:47:05 -0700

    Add new gateway command

---
+++ cmd/gateway/new.go
@@ -0,0 +1,37 @@
+package gateway
+
+import (
+	"fmt"
+
+	"github.com/funcgql/cli/cmd/flag"
+	"github.com/funcgql/cli/config"
+	"github.com/funcgql/cli/gateway"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var newCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Create a new Apollo gateway for the deploy target(s)",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		functionTypes := flag.TargetFunctionTypes()
+		if len(functionTypes) <= 0 {
+			return errors.New("at least one cloud function type flag must be specified")
+		}
+
+		cfg, err := config.LoadFromRepoRoot()
+		if err != nil {
+			return err
+		}
+
+		for _, functionType := range functionTypes {
+			fmt.Println("🌉 Generating", functionType, "Apollo gateway")
+			newGateway := gateway.New(functionType)
+			if err := newGateway.ExportIndexFile(cfg); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	},
+}
