37d8028ef6d1757ce2d85686bb6aea5dbae5f969
Yi Wang <neakor@gmail.com>
2022-04-23 16:28:15 -0700

    Allow retrieving module in the current working directory

--- go/module/get.go
+++ go/module/get.go
@@ -1,10 +1,12 @@
 package module
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/funcgql/cli/cliio"
 	"github.com/funcgql/cli/config"
+	"github.com/funcgql/cli/shell"
 	"github.com/pkg/errors"
 )
 
@@ -22,5 +24,25 @@ func WithName(name string, cfg *config.Config) (Module, bool, error) {
 		name:    name,
 		dirName: dirName,
 		absPath: absPath,
+	}, true, nil
+}
+
+func CurrentDir() (Module, bool, error) {
+	workingDirPath, err := os.Getwd()
+	if err != nil {
+		return nil, false, errors.Wrap(err, "failed to determine current working directory path")
+	}
+
+	dirName := filepath.Base(workingDirPath)
+
+	output, err := shell.ExecuteIn(workingDirPath, "go", "list", "-m")
+	if err != nil {
+		return nil, false, errors.Wrapf(err, "failed to obtain module name in %s", workingDirPath)
+	}
+
+	return module{
+		name:    output.Combined,
+		dirName: dirName,
+		absPath: workingDirPath,
 	}, true, nil
 }
