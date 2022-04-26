37e19c545bba10c2d45d9b741e8b4252508b44ce
Yi Wang <neakor@gmail.com>
2022-04-26 10:42:19 -0700

    Add install function to tool object

--- go/module/tools.go
+++ go/module/tools.go
@@ -1,15 +1,18 @@
 package module
 
 import (
+	"fmt"
 	"path/filepath"
 	"regexp"
 
 	"github.com/funcgql/cli/cliio"
+	"github.com/funcgql/cli/shell"
 	"github.com/pkg/errors"
 )
 
 type Tool struct {
-	ImportPath string
+	ImportPath    string
+	moduleAbsPath string
 }
 
 func (m module) Tools() ([]Tool, error) {
@@ -25,9 +28,21 @@ func (m module) Tools() ([]Tool, error) {
 	matches := importPathRegex.FindAllStringSubmatch(content, -1)
 	for _, match := range matches {
 		if len(match) > 1 {
-			results = append(results, Tool{ImportPath: match[1]})
+			results = append(results, Tool{
+				ImportPath:    match[1],
+				moduleAbsPath: m.absPath,
+			})
 		}
 	}
 
 	return results, nil
+}
+
+func (t Tool) Install(version string, shellAPI shell.API) error {
+	versionedImportPath := fmt.Sprintf("%s@%s", t.ImportPath, version)
+	if output, err := shellAPI.ExecuteIn(t.moduleAbsPath, "go", "install", versionedImportPath); err != nil {
+		return errors.Wrapf(err, "failed to install %s in %s %s", versionedImportPath, t.moduleAbsPath, output.Combined)
+	}
+
+	return nil
 }
--- go/module/tools_test.go
+++ go/module/tools_test.go
@@ -3,6 +3,9 @@ package module
 import (
 	"testing"
 
+	"github.com/funcgql/cli/shell"
+	"github.com/funcgql/cli/shell/mocks"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,7 +18,27 @@ func Test_Tools(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, []Tool{
 		{
-			ImportPath: "github.com/golang/mock/mockgen",
+			ImportPath:    "github.com/golang/mock/mockgen",
+			moduleAbsPath: m.absPath,
 		},
 	}, results)
+}
+
+func Test_Install(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	targetTool := Tool{
+		ImportPath:    "IMPORT_PATH",
+		moduleAbsPath: "MODULE_PATH",
+	}
+
+	shellAPI := mocks.NewMockAPI(mockCtrl)
+	shellAPI.EXPECT().
+		ExecuteIn("MODULE_PATH", "go", "install", "IMPORT_PATH@VERSION").
+		Return(shell.Output{}, nil)
+
+	err := targetTool.Install("VERSION", shellAPI)
+
+	require.NoError(t, err)
 }
